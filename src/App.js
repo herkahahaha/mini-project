b3c728fcc96c0d94498d767076c0f87cf11ad7e3
Herka Trya Fitra <herkahahaha@gmail.com>
2021-03-12 18:44:09 +0700

    transform svg to js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./components/icons/icon/header.svg";
+// import Banner from "./components/icons/banner";
+import Clipboard from "./components/icons/clipboard";
+import Idea from "./components/icons/idea";
+import Rewards from "./components/icons/rewards";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-header__content">
+          <h2>The Easiest Way to Bring Employees Up to Speed </h2>
+          <button>hitme</button>
+        </div>
+        <img src={Banner} className="App-header__logo" alt="logo" />
       </header>
+      <section>
+        <h1>Lets cards</h1>
+        <Clipboard />
+        <Rewards />
+        <Idea />
+        <h1>Lets section body</h1>
+        <h1>Lets section we here</h1>
+        <h1>Lets section growth</h1>
+        <h1>footer</h1>
+      </section>
     </div>
   );
 }
---
+++ src/components/icons/banner.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+// import BannerImg from './icon/banner.svg'
+
+const Banner = () => {
+  return <p></p>;
+};
+export default Banner;
